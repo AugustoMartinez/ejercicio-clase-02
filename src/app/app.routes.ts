0432bb9f91bf9ae3391d86665d74f063688fac52
Augusto Martinez <augustomartinezt5@gmail.com>
2024-04-23 21:58:29 -0300

    Validacion de Logeo y Redireccion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { ErrorComponent } from './componentes/error/error.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/inicio', pathMatch: "full" },
-    { path: 'inicio', component: BienvenidoComponent },
+    { path: 'inicio', component: InicioComponent },
+    { path: 'bienvenido', component: BienvenidoComponent },
     { path: 'login', component: LoginComponent},
     { path: '**', component: ErrorComponent }
 ];
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -3,11 +3,12 @@ import { Usuario } from '../../modelos/usuario';
 import { FormsModule } from '@angular/forms';
 import { BienvenidoComponent } from '../bienvenido/bienvenido.component';
 import { ErrorComponent } from '../error/error.component';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, BienvenidoComponent, ErrorComponent],
+  imports: [FormsModule, BienvenidoComponent, ErrorComponent,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -16,6 +17,19 @@ export class LoginComponent {
   usuario : string = ""
   clave : string = ""
 
+  constructor(private router: Router) {
 
   }
+
+  goTo(path: string) {
+    this.router.navigate([path]);
+  }
+
+  validarUsuario(){
+    if(this.usuario == this.user.nombre && this.clave == this.user.clave){
+      this.goTo("bienvenido")
+    }
+  }
+
+}
 
